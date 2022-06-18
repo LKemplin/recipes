9de200077ffdce543a9c775d3942b188b1a1ca52
Lynsey Kemplin <purplez479@gmail.com>
2022-06-18 16:16:05 -0700

    updated with "user_id" in session requirements

--- flask_app/controllers/recipes.py
+++ flask_app/controllers/recipes.py
@@ -5,10 +5,14 @@ from flask_app.models.user import User
 
 @app.route("/recipes/new")
 def create_recipe():
+    if "user_id" not in session:
+        return redirect("/logout")
     return render_template("create.html", messages = get_flashed_messages())
 
 @app.route("/recipes/save", methods=['POST'])
 def save_recipe():
+    if "user_id" not in session:
+        return redirect("/logout")
     if not Recipe.validate_recipe(request.form):
         return redirect("/dashboard")
     data = {
@@ -24,22 +28,32 @@ def save_recipe():
 
 @app.route("/recipes/<int:id>")
 def view_instructions(id):
+    if "user_id" not in session:
+        return redirect("/logout")
     data = {"id": id}
     this_user = User.get_user_by_id({"id": session["user_id"]})
     return render_template("recipe_instructions.html", this_user = this_user, recipe = Recipe.get_one(data))
 
 @app.route("/recipes/delete/<int:id>")
 def delete_recipe(id):
+    if "user_id" not in session:
+        return redirect("/logout")
     data = {"id": id}
     Recipe.delete(data)
     return redirect("/dashboard")
 
 @app.route("/recipes/edit/<int:id>")
 def edit_recipe(id):
+    if "user_id" not in session:
+        return redirect("/logout")
     data = {"id": id}
     return render_template("edit_recipe.html", recipe = Recipe.get_one(data))
 
 @app.route("/recipes/update", methods=["POST"])
 def update_recipe():
+    if "user_id" not in session:
+        return redirect("/logout")
+    if not Recipe.validate_recipe:
+        return redirect("/dashboard")
     Recipe.update(request.form)
     return redirect("/dashboard")
